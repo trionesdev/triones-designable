7b19f9456ad1ccc66dfaa16e23fb008b2f95e35c
bane.shi <fengxiaotx@163.com>
2023-07-01 15:46:03 +0800

    feat: init

---
+++ examples/basic/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {Designer} from "@alkaid/designer";
+
+function App() {
+  return (
+    <div className="App">
+      <Designer/>
+
+    </div>
+  );
+}
+
+export default App;
