755f88b3267ae56b575dd649cd5e61bedbdc2cd0
bane.shi <fengxiaotx@163.com>
2023-08-08 21:40:25 +0800

    feat: change styles from less to css-in-js

--- examples/basic/src/alkaid/react/widgets/ResourceWidget/index.tsx
+++ examples/basic/src/alkaid/react/widgets/ResourceWidget/index.tsx
@@ -1,110 +1,112 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import {
-  isResourceHost,
-  isResourceList,
-  IResourceLike,
-  IResource,
+    isResourceHost,
+    isResourceList,
+    IResourceLike,
+    IResource,
 } from '@alkaid/core'
-import { isFn } from '@alkaid/shared'
-import { observer } from '@formily/reactive-react'
-import { usePrefix } from '../../hooks'
-import { IconWidget } from '../IconWidget'
-import { TextWidget } from '../TextWidget'
+import {isFn} from '@alkaid/shared'
+import {observer} from '@formily/reactive-react'
+import {useCssInJs, usePrefix} from '../../hooks'
+import {IconWidget} from '../IconWidget'
+import {TextWidget} from '../TextWidget'
 import cls from 'classnames'
-import './styles.less'
+// import './styles.less'
+import {genResourceWidgetStyle} from "./styles";
 
 export type SourceMapper = (resource: IResource) => React.ReactChild
 
 export interface IResourceWidgetProps {
-  title: React.ReactNode
-  sources?: IResourceLike[]
-  className?: string
-  defaultExpand?: boolean
-  children?: SourceMapper | React.ReactElement
+    title: React.ReactNode
+    sources?: IResourceLike[]
+    className?: string
+    defaultExpand?: boolean
+    children?: SourceMapper | React.ReactElement
 }
 
 export const ResourceWidget: React.FC<IResourceWidgetProps> = observer(
-  (props) => {
-    const prefix = usePrefix('resource')
-    const [expand, setExpand] = useState(props.defaultExpand)
-    const renderNode = (source: IResource) => {
-      const { node, icon, title, thumb, span } = source
-      return (
-        <div
-          className={prefix + '-item'}
-          style={{ gridColumnStart: `span ${span || 1}` }}
-          key={node.id}
-          data-designer-source-id={node.id}
-        >
-          {thumb && <img className={prefix + '-item-thumb'} src={thumb} />}
-          {icon && React.isValidElement(icon) ? (
-            <>{icon}</>
-          ) : (
-            <IconWidget
-              className={prefix + '-item-icon'}
-              infer={icon}
-              style={{ width: 150, height: 40 }}
-            />
-          )}
-          <span className={prefix + '-item-text'}>
+    (props) => {
+        const prefix = usePrefix('resource')
+        const {hashId, wrapSSR} = useCssInJs({prefix, styleFun: genResourceWidgetStyle})
+        const [expand, setExpand] = useState(props.defaultExpand)
+        const renderNode = (source: IResource) => {
+            const {node, icon, title, thumb, span} = source
+            return (
+                <div
+                    className={cls(prefix + '-item', hashId)}
+                    style={{gridColumnStart: `span ${span || 1}`}}
+                    key={node.id}
+                    data-designer-source-id={node.id}
+                >
+                    {thumb && <img className={cls(prefix + '-item-thumb', hashId)} src={thumb}/>}
+                    {icon && React.isValidElement(icon) ? (
+                        <>{icon}</>
+                    ) : (
+                        <IconWidget
+                            className={cls(prefix + '-item-icon', hashId)}
+                            infer={icon}
+                            style={{width: 150, height: 40}}
+                        />
+                    )}
+                    <span className={cls(prefix + '-item-text', hashId)}>
             {
-              <TextWidget>
-                {title || node.children[0]?.getMessage('title')}
-              </TextWidget>
+                <TextWidget>
+                    {title || node.children[0]?.getMessage('title')}
+                </TextWidget>
             }
           </span>
-        </div>
-      )
+                </div>
+            )
+        }
+        const sources = props.sources.reduce<IResource[]>((buf, source) => {
+            if (isResourceList(source)) {
+                return buf.concat(source)
+            } else if (isResourceHost(source)) {
+                return buf.concat(source.Resource)
+            }
+            return buf
+        }, [])
+        const remainItems =
+            sources.reduce((length, source) => {
+                return length + (source.span ?? 1)
+            }, 0) % 3
+        return wrapSSR(
+            <div
+                className={cls(prefix, props.className, {
+                    expand,
+                }, hashId)}
+            >
+                <div
+                    className={cls(prefix + '-header', hashId)}
+                    onClick={(e) => {
+                        e.stopPropagation()
+                        e.preventDefault()
+                        setExpand(!expand)
+                    }}
+                >
+                    <div className={cls(prefix + '-header-expand', hashId)}>
+                        <IconWidget infer="Expand" size={10}/>
+                    </div>
+                    <div className={cls(prefix + '-header-content', hashId)}>
+                        <TextWidget>{props.title}</TextWidget>
+                    </div>
+                </div>
+                <div className={cls(prefix + '-content-wrapper', hashId)}>
+                    <div className={cls(prefix + '-content', hashId)}>
+                        {sources.map(isFn(props.children) ? props.children : renderNode)}
+                        {remainItems ? (
+                            <div
+                                className={cls(prefix + '-item-remain', hashId)}
+                                style={{gridColumnStart: `span ${3 - remainItems}`}}
+                            ></div>
+                        ) : null}
+                    </div>
+                </div>
+            </div>
+        )
     }
-    const sources = props.sources.reduce<IResource[]>((buf, source) => {
-      if (isResourceList(source)) {
-        return buf.concat(source)
-      } else if (isResourceHost(source)) {
-        return buf.concat(source.Resource)
-      }
-      return buf
-    }, [])
-    const remainItems =
-      sources.reduce((length, source) => {
-        return length + (source.span ?? 1)
-      }, 0) % 3
-    return (
-      <div
-        className={cls(prefix, props.className, {
-          expand,
-        })}
-      >
-        <div
-          className={prefix + '-header'}
-          onClick={(e) => {
-            e.stopPropagation()
-            e.preventDefault()
-            setExpand(!expand)
-          }}
-        >
-          <div className={prefix + '-header-expand'}>
-            <IconWidget infer="Expand" size={10} />
-          </div>
-          <div className={prefix + '-header-content'}>
-            <TextWidget>{props.title}</TextWidget>
-          </div>
-        </div>
-        <div className={prefix + '-content-wrapper'}>
-          <div className={prefix + '-content'}>
-            {sources.map(isFn(props.children) ? props.children : renderNode)}
-            {remainItems ? (
-              <div
-                className={prefix + '-item-remain'}
-                style={{ gridColumnStart: `span ${3 - remainItems}` }}
-              ></div>
-            ) : null}
-          </div>
-        </div>
-      </div>
-    )
-  }
 )
 
 ResourceWidget.defaultProps = {
-  defaultExpand: true,
+    defaultExpand: true,
 }
--- examples/basic/src/alkaid/react/widgets/ResourceWidget/styles.ts
+++ examples/basic/src/alkaid/react/widgets/ResourceWidget/styles.ts
@@ -7,8 +7,78 @@ export const genResourceWidgetStyle = (
 ): CSSInterpolation => [
     {
         [`.${prefixCls}`]: {
-            minHeight: '100%',
-            minWidth: '100%'
+            flexWrap: 'wrap',
+            overflow: 'hidden',
+            [`&-header`]: {
+                display: 'flex',
+                alignItems: 'center',
+                padding: '5px 8px',
+                color: '#333',
+                borderBottom: `1px solid #d9d9d9`,
+                backgroundColor: `#eee`,
+                cursor: 'pointer',
+                transition: 'all 0.25s ease-in-out',
+                fontSize: '13px',
+                [`&-expand`]: {
+                    transform: 'rotate(-90deg)',
+                    fontSize: '12px',
+                    transition: 'all 0.15s ease-in-out',
+                    marginRight: '3px'
+                }
+            },
+            [`&-content-wrapper`]: {
+                display: 'flex',
+                justifyContent: 'center',
+                background: '#fff'
+            },
+            [`&-content`]: {
+                width: '100%',
+                // display: 'flex',
+                flexWrap: 'wrap',
+                display: 'none'
+            },
+            [`&.expand`]: {
+                [`.dn-resource-content`]: {
+                    display: 'grid',
+                    gridTemplateColumns: 'repeat(3, 33.3333%)',
+                    gridGap: '1px',
+                    backgroundColor: '#d9d9d9',
+                    borderBottom: `1px solid #d9d9d9`
+                },
+                [`.dn-resource-header-expand`]: {
+                    transform: 'rotate(0)'
+                }
+            },
+            [`&-item`]: {
+                position: 'relative',
+                userSelect: 'none',
+                background: '#fff',
+                minHeight: '40px',
+                color: '#333',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                flexDirection: 'column',
+                cursor: 'grab',
+                transition: 'color 0.1s ease-out',
+                [`&:hover`]: {
+                    color: `rgba(24, 144, 255, 1)`,
+                    boxShadow: ' 0 0 10px rgba(0, 0, 0, 0.1)',
+                    zIndex: 1
+                },
+                [`&-icon`]: {
+                    margin: '12px 0'
+                },
+                [`&-text`]: {
+                    textAlign: 'center',
+                    fontSize: '12px',
+                    lineHeight: 1,
+                    marginBottom: '12px'
+                },
+                [`&-remain`]:{
+                    background:'#fff'
+                }
+            }
         }
     }
 ]
