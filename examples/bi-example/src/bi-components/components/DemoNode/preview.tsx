9068c518fe013af9ce1052649a5d1622508d861e
gzxmac <wanan499gzx@163.com>
2023-09-13 09:23:33 +0800

    feat: demo组件调试

--- examples/bi-example/src/bi-components/components/DemoNode/preview.tsx
+++ examples/bi-example/src/bi-components/components/DemoNode/preview.tsx
@@ -2,15 +2,65 @@ import { createBehavior, createResource } from "@alkaid/core";
 import {
   AllLocales,
   AllSchemas,
-  createVoidFieldSchema,
+  // createVoidFieldSchema,
 } from "@alkaid/formily-antd";
 import { DnFC } from "@alkaid/react";
 import { observer } from "@formily/reactive-react";
+import { Pie, PieConfig } from "@ant-design/charts";
 
 import React from "react";
+import { createVoidFieldSchema } from "../FloatWrapper";
 
 export const DemoNode: DnFC<React.ComponentProps<any>> = observer((props) => {
   // console.log(props);
+  const data = [
+    {
+      type: "分类一",
+      value: 27,
+    },
+    {
+      type: "分类二",
+      value: 25,
+    },
+    {
+      type: "分类三",
+      value: 18,
+    },
+    {
+      type: "分类四",
+      value: 15,
+    },
+    {
+      type: "分类五",
+      value: 10,
+    },
+    {
+      type: "其他",
+      value: 5,
+    },
+  ];
+  const config: PieConfig = {
+    appendPadding: 10,
+    data,
+    angleField: "value",
+    colorField: "type",
+    radius: 0.9,
+    label: {
+      type: "inner",
+      offset: "-30%",
+      content: ({ percent }: any) => `${(percent * 100).toFixed(0)}%`,
+      style: {
+        fontSize: 14,
+        textAlign: "center",
+      },
+    },
+    interactions: [
+      {
+        type: "element-active",
+      },
+    ],
+  };
+  console.log(props);
   return (
     <div
       {...props}
@@ -21,6 +71,9 @@ export const DemoNode: DnFC<React.ComponentProps<any>> = observer((props) => {
       // data-content-editable={"x-component-props.content"}
     >
       demo{props?.content}
+      <div style={{ width: 300, height: 300 }}>
+        <Pie {...config} supportCSSTransform />
+      </div>
     </div>
   );
 });
